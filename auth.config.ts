18aaf01840a2104b4741e270e7cd4cd82a038b73
marcus <marcus.chui.ca@icloud.com>
2024-09-06 05:13:03 -0400

    fix(auth): adjust spacing in comment and add custom logger to handle credential sign-in errors

--- auth.config.ts
+++ auth.config.ts
@@ -10,7 +10,7 @@ export const authConfig = {
             const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
             if (isOnDashboard) {
                 return isLoggedIn;
-                 // Redirect unauthenticated users to login page
+                // Redirect unauthenticated users to login page
             } else if (isLoggedIn) {
                 return Response.redirect(new URL('/dashboard', nextUrl));
             }
@@ -30,4 +30,9 @@ export const authConfig = {
         },
     },
     providers: [], // Add providers with an empty array for now
+    logger: {
+        error(code, ...message) {
+            if (code.name !== 'CredentialsSignin') console.error(code, ...message);
+        },
+    },
 } satisfies NextAuthConfig;
